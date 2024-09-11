97e6afef131f54ed0bbe23bdeb124043a72165f6
reshmasatpute <81470981+reshmasatpute@users.noreply.github.com>
2024-09-11 14:51:18 +0530

    Delete Java-Technical-Task1/Java-Technical-Task/task directory

--- Java-Technical-Task1/Java-Technical-Task/task/src/main/java/com/tech/task/Exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tech.task.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = -4169528211739042556L;
-
-	public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
